85ce3cc4a5ce914222408ca4c02acf8a00fb97a2
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2021-08-26 13:34:25 +1000

    Create a database context class and connection string in a configuration file. Note: config.json is not in the repo.

--- Startup.cs
+++ Startup.cs
@@ -1,8 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Session05ArchitectureMVC.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +16,20 @@ namespace Session05ArchitectureMVC
     public class Startup
     {
         // This file is the entry point of our project.
-        
+
+        // Declare a configuration variable.
+        private readonly IConfigurationRoot configuration;
+
+        // Create a constructor for configuration information.
+        public Startup(IWebHostEnvironment env)
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddEnvironmentVariables()
+                // We can get the content root from the env object
+                .AddJsonFile(env.ContentRootPath + "/config.json")
+                .Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -25,6 +41,13 @@ namespace Session05ArchitectureMVC
             // - Microsoft.EntityFrameworkCore 
             // - Microsoft.AspNetCore.Identity.EntityFrameworkCore
             // - Microsoft.EntityFrameworkCore.SqlServer
+            services.AddDbContext<TableDataContext>(options =>
+            {
+                // Retrieve the connection string from the config.json file.
+                var connectionString = configuration.GetConnectionString("PostConnection");
+                // using Microsoft.EntityFrameworkCore;
+                options.UseSqlServer();
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
