983e700c209a1b778aed352bc0be0c4f63f1f1c6
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2021-09-14 09:45:59 +1000

    Create a view for CreateRole.

--- Controllers/AdministrationController.cs
+++ Controllers/AdministrationController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Session05ArchitectureMVC.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,20 @@ namespace Session05ArchitectureMVC.Controllers
         private readonly UserManager<IdentityUser> userManager;
         private readonly RoleManager<IdentityRole> roleManager;
         
+        public AdministrationController(UserManager<IdentityUser> _userManager,
+            RoleManager<IdentityRole> _roleManager)
+        {
+            // Pass in the user and role managers.
+            userManager = _userManager;
+            roleManager = _roleManager;
+        }
+
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            return View(new CreateRoleViewModel());
+        }
+
         public IActionResult Index()
         {
             return View();
